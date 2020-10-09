bbbbeb3ca7d6226110d643f0d5f7f0aef1086339
willianfrancas <willianfrancas@gmail.com>
2020-10-08 23:41:19 -0300

    subitems adicionados ao menu

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-if(window.location.pathname === "/") {
-    window.location = "./grid-system.html";
+const pathName = window.location.pathname;
+if (pathName === "/" || pathName === "/index.html") {
+  window.location = "./grid-system.html";
 }
 
 // var url = 'https://api.sheety.co/97717c63f8de6e1e58e42fca477f2c78/itemsMenu/menu'
@@ -8,37 +9,46 @@ var url = 'assets/data/menu.json'
 
 var xhttp = new XMLHttpRequest();
 
-  xhttp.open('GET',url,true)
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      var res = xhttp.responseText
-      renderMenu(res);
-    }
+xhttp.open('GET', url, true)
+xhttp.onreadystatechange = function () {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+    var res = xhttp.responseText
+    renderMenu(res);
   }
-  xhttp.send();
+}
+xhttp.send();
 
-  function renderMenu(res) {
-      const menu = JSON.parse(res).menu;
-      const elUl = document.querySelector('.render');
-      const render = menu.map(item => {
-          let list = renderItems(item);
-          elUl.appendChild(list);
+function renderMenu(res) {
+  const menu = JSON.parse(res).menu;
+  const elUl = document.querySelector('.render');
+  const render = menu.map(item => {
+
+    let list = renderItems(item);
+    elUl.appendChild(list);
+  });
+}
+
+function renderItems(item) {
+
+  const elLi = document.createElement('li');
+  const elA = document.createElement('a');
+  elA.setAttribute('href', item.path);
+  elA.innerHTML = item.name;
+  elLi.appendChild(elA);
+
+  if (item.childrens) {
+    const elSubUl = document.createElement('ul');
+      item.childrens.map(subitem => {
+        let sublist = renderItems(subitem);
+          elSubUl.appendChild(sublist);
       });
+      elLi.appendChild(elSubUl);
   }
-
-  function renderItems(item) {
-      
-    const elLi = document.createElement('li');
-    const elA = document.createElement('a');
-          elA.setAttribute('href', item.path);
-          elA.innerHTML = item.name;
-          elLi.appendChild(elA);
-
-    return elLi;
-  }
+  return elLi;
+}
 
 $(() => {
-    $('#btn-close-all').on('click', e => {
-        $('.alert').fadeOut();
-    });
+  $('#btn-close-all').on('click', e => {
+    $('.alert').fadeOut();
+  });
 });
